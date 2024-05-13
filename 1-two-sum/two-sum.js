6cbe51747a27704a3335eb6eca71b74d10841c0c
Maksym Mezko <159728371+Maksym-Mezko@users.noreply.github.com>
2024-05-13 18:16:58 +0300

    Time: 66 ms (54.19%) | Memory: 51.8 MB (15.74%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,17 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    const map = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        map.set(nums[i], i);
-    }
-
+    const map = new Map()
     for (let i = 0; i < nums.length; i++) {
         const a = nums[i];
         const b = target - a;
-        const ii = map.get(b);
-        if (ii && i !== ii) {
-            return [i, ii]
+        if (map.has(b)) {
+            return [i, map.get(b)]
+        } else {
+            map.set(a, i);
         }
     }
 };
